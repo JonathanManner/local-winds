8f25b03bd4306acfc729850989a6c4d85807d140
Jonathan Manner <Jonathanmanner@hotmail.com>
2021-12-15 09:22:19 +0100

    push version 1

--- src/features/RenderTomorrow/RenderTomorrow.js
+++ src/features/RenderTomorrow/RenderTomorrow.js
@@ -14,18 +14,6 @@ export const RenderTomorrow = (props) => {
   const isValidDate = (date) => weather[date].length >= 4;
   const dates = Object.keys(weather).slice(1).filter(isValidDate);
 
-  // const weekday = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag",]
-  // const getDayOfWeek = (daysToAdd = 0) => {
-  //   let days = daysToAdd;
-  //   const day = new Date().getDay();
-  //   if (days + day > weekday.length) {
-  //     days = days + day - weekday.length;
-  //   } else {
-  //     days = daysToAdd;
-  //   }
-  //   return weekday[days + day];
-  // }
-
   if (
     !weather ||
     typeof weather !== "object" ||
